175ef77d63890dd1bac2296ef0f9a427935facb5
josuebmurillo <41100076+josuebmurillo@users.noreply.github.com>
2020-06-24 01:02:32 -0600

    Grafica
    Se logro graficar los latidos

--- HolaMiniWin-windows/main.cpp
+++ HolaMiniWin-windows/main.cpp
@@ -1,11 +1,52 @@
-
 #include "miniwin.h"
+#include <time.h>
+#include <Persona.h>
+#include <vector>
+//#include <string>
 using namespace miniwin;
+using namespace std;
 
-int main() {
-   vredimensiona(400, 300);
-   rectangulo(100, 100, 300, 200);
-   texto(152, 142, "Hola, MiniWin!");
-   refresca();
-   return 0;
+int main()
+{
+    Persona per;
+    vector<float> registro;
+    registro.resize(1000);
+
+    for (int n = 0; n < 0; n++)
+        registro[n] = 0;
+
+    per.estarEnfermo();
+    vredimensiona(1000, 600);
+    //rectangulo(100, 100, 300, 200);
+    texto(0,10,"Tempaetura:");
+    refresca();
+    color(ROJO);
+    linea(0, 300, 1000, 300);
+
+    int counter = 0;
+    //while(true)
+    //{
+    int ultimo = 0;
+    int teclas = 0;
+    int i = 0;
+    while(teclas != int('Q'))
+    {
+            int pue = int(per.getPulso(float(i%1000)));
+            color(VERDE);
+            linea((i)-1, 300+(ultimo),(i),300+(pue));
+            refresca();
+            espera(1);
+            ultimo = pue;
+            i++;
+
+        teclas = tecla();
+    }
+
+
+
+
+
+
+    return 0;
 }
+
--- HolaMiniWin-windows/src/Persona.cpp
+++ HolaMiniWin-windows/src/Persona.cpp
@@ -15,7 +15,7 @@ Persona::~Persona()
 
 void Persona::estarSano()
 {
-    indiceTemp = 0;
+    indiceTemp = 1;
     indicePulso = 1;
 }
 
@@ -45,37 +45,57 @@ void Persona::casoExtremoinferior()
 
 float Persona::getPulso(float tiempo)
 {
-    float x = float((int(tiempo) + int(indicePulso))%10);
+    float x =float((int(tiempo*indicePulso))%100);
     float res = 0;
-    if(x<1)
-        {
-            res = 0;
-        }
-    else if (1<= x && x < 2)
-        {
-            res = (-2*x) + 2;
-        }
-    else if (2<= x && x < 4)
-        {
-            res = (4*x) + 28;
-        }
-    else if (4<= x && x < 5.5)
-        {
-            res = (-8 * x) + 38;
-        }
-    else if (5.5<= x && x < 6.5)
-        {
-            res = (8 * x) + 50;
-        }
-    else if (6.5<= x && x < 7)
-        {
-            res = (-4 * x) + 28;
-        }
-    else if (7<= x && x < 9)
-        {
-            res = 0;
-        }
-        return res;
+    if(0<= x && x<10)
+    {
+        res = 0;
+    }
+    else if (10<= x && x < 15)
+    {
+        res = x - 10;
+    }
+    else if (15<= x && x < 20)
+    {
+        res = -(x) + 20;
+    }
+    else if (20<= x && x < 25)
+    {
+        res = (-2 * x)+40;
+    }
+    else if (25<= x && x < 30)
+    {
+        res = (2*x)-60;
+    }
+    else if (30<= x && x < 40)
+    {
+        res = (4*x)-140;
+    }
+    else if (40<= x && x < 50)
+    {
+        res = (-6 * x) + 280;
+    }
+    else if (50<= x && x < 55)
+    {
+        res = (4 * x) - 220;
+    }
+    else if (55<= x && x < 60)
+    {
+        res = 0;
+    }
+    else if (60<= x && x < 65)
+    {
+        res = x - 60;
+    }
+    else if (65<= x && x < 70)
+    {
+        res = -x + 70;
+    }
+    else if (70<= x)
+    {
+        res = 0;
+    }
+    return -res;
 }
 
 float Persona::getTemperatura()
